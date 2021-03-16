6c3b4fdb11dac1ac1b798fee334084a88c902ef3
hannile <68334996+hannile@users.noreply.github.com>
2021-03-16 16:00:45 +0100

    update index

--- script/main.js
+++ script/main.js
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:fc61c6179dfcdf016fc89e331dfa2adc678e6d1021b21305251e26d37ebd9e56
-size 36
+$(document).ready(function(){
+
+
+});
--- script/show.js
+++ script/show.js
@@ -1,3 +1,52 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:07dda0f6c4388ad350408b237cc93f658209e833bf240b9bb66f9cced45a5a38
-size 1486
+/*ProjectNav*/
+$(document).ready(function(){
+
+    $(".projects a").click(function(){ 
+      var project = $(this).attr('href');
+      $(".project-item").removeClass("is-visible");
+      $(project+'').addClass("is-visible");
+      /*$(".information").removeClass("is-visible");
+        */
+       const vw = window.innerHeight;
+       const listHeight = $(project + " .list").outerHeight();
+       const paddingTop = vw - listHeight;
+       $(".content").css({
+           "padding-top": 170
+          //  "padding-bottom": listHeight + "px"
+        });
+       
+    });
+    /*ProjectNav*/
+    $(".headers a").click(function(){
+    /* $("#info").toggle();*/
+    });
+
+
+    //------- SCROLL ---------//
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+    
+
+
+});
+  
